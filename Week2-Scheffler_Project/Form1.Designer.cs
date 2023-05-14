25fe8f85a95d3a75735e9e5a5d159a186d680951
zscheff <zscheff@2343-vNet-1-0>
2023-05-14 12:35:40 -0500

    Add project files.

---
+++ Week2-Scheffler_Project/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿
+namespace Week2_Scheffler_Project
+{
+    partial class frmSummer2023
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.cnclButton = new System.Windows.Forms.Button();
+            this.clsButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.Honeydew;
+            this.btnClear.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnClear.Location = new System.Drawing.Point(45, 339);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(141, 90);
+            this.btnClear.TabIndex = 0;
+            this.btnClear.Text = "&Wipe Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.Aqua;
+            this.btnSave.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnSave.Location = new System.Drawing.Point(240, 339);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(141, 90);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "&Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // cnclButton
+            // 
+            this.cnclButton.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.cnclButton.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.cnclButton.ForeColor = System.Drawing.Color.Wheat;
+            this.cnclButton.Location = new System.Drawing.Point(435, 339);
+            this.cnclButton.Name = "cnclButton";
+            this.cnclButton.Size = new System.Drawing.Size(141, 90);
+            this.cnclButton.TabIndex = 2;
+            this.cnclButton.Text = "Can&cel";
+            this.cnclButton.UseVisualStyleBackColor = false;
+            this.cnclButton.Click += new System.EventHandler(this.cnclButton_Click);
+            // 
+            // clsButton
+            // 
+            this.clsButton.BackColor = System.Drawing.Color.Wheat;
+            this.clsButton.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.clsButton.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.clsButton.Location = new System.Drawing.Point(630, 339);
+            this.clsButton.Name = "clsButton";
+            this.clsButton.Size = new System.Drawing.Size(141, 90);
+            this.clsButton.TabIndex = 3;
+            this.clsButton.Text = "&Exit";
+            this.clsButton.UseVisualStyleBackColor = false;
+            this.clsButton.Click += new System.EventHandler(this.clsButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(45, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Enter text here:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(192, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(579, 32);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Javanese Text", 32.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(45, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(726, 273);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "a";
+            // 
+            // frmSummer2023
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SeaGreen;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.clsButton);
+            this.Controls.Add(this.cnclButton);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnClear);
+            this.Name = "frmSummer2023";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Summer 2023, Scheffler";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button cnclButton;
+        private System.Windows.Forms.Button clsButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Week2-Scheffler_Project/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+    Zach Scheffler
+    ITD-2343 Obj-Oriented Programming Using C#
+    16 May, 2023
+*/
+namespace Week2_Scheffler_Project
+{
+    public partial class frmSummer2023 : Form
+    {
+        public frmSummer2023()
+        {
+            InitializeComponent();
+
+            label2.Text = "";
+
+            //Ensures that the large text box is empty on start
+
+        }
+
+        private void clsButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+
+            //Uses library function to close form out
+        }
+
+        private void cnclButton_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            label2.Text = textBox1.Text;
+
+            //Saves text from textBox1 into label2
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            label2.Text = "";
+            textBox1.Text = "";
+        }
+
+        
+    }
+}
